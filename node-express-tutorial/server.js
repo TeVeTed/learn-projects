d855a023897fe0f651cb6308c8f8a869c25a7c00
o.tiapkin <o.tiapkin@temabit.com>
2019-11-03 16:38:04 +0200

    react with hooks and TS

--- node-express-tutorial/server.js
+++ node-express-tutorial/server.js
@@ -14,16 +14,16 @@ app.use(express.json());
 
 const start = (req, res) => res.status(200).send({ 'message': 'SUCCESS' });
 
-app.get('/', start);
-app.post('/api/v1/reflections', Auth.verifyToken, Reflection.create);
-app.get('/api/v1/reflections', Auth.verifyToken, Reflection.getAll);
-app.get('/api/v1/reflections/:id', Auth.verifyToken, Reflection.getOne);
-app.put('/api/v1/reflections/:id', Auth.verifyToken, Reflection.update);
+app.get(   '/',                                                     start);
+app.post(  '/api/v1/reflections',     Auth.verifyToken, Reflection.create);
+app.get(   '/api/v1/reflections',     Auth.verifyToken, Reflection.getAll);
+app.get(   '/api/v1/reflections/:id', Auth.verifyToken, Reflection.getOne);
+app.put(   '/api/v1/reflections/:id', Auth.verifyToken, Reflection.update);
 app.delete('/api/v1/reflections/:id', Auth.verifyToken, Reflection.delete);
-app.get('/api/v1/users', UserWithDb.getAll);
-app.post('/api/v1/users', UserWithDb.create);
-app.post('/api/v1/users/login', UserWithDb.login );
-app.delete('/api/v1/users/me', Auth.verifyToken, UserWithDb.delete);
+app.get(   '/api/v1/users',                             UserWithDb.getAll);
+app.post(  '/api/v1/users',                             UserWithDb.create);
+app.post(  '/api/v1/users/login',                       UserWithDb.login );
+app.delete('/api/v1/users/me',        Auth.verifyToken, UserWithDb.delete);
 
 app.listen(3000);
 console.log('app running on port ', 3000);
---
+++ react-hooks-ts/src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import './App.css';
+
+interface IAppProps {
+  name: string;
+  site: string;
+}
+
+const App: React.FC<IAppProps> = props => {
+  return (
+    <div className="container">
+      <h1>TZ #1 with hooks & TypeScript</h1>
+      <nav>
+        <p>Навигация</p>
+      </nav>
+      
+      <p>Отрисовка роутов</p>
+      <p>Привет, {props.name}</p>
+      <p>Сайт: {props.site}</p>
+      {props.children}
+    </div>
+  );
+}
+
+const Baby = () => {
+  return <p>дочерний элемент</p> 
+};
+
+const RoutedApp = () => {
+  return <App name="Frontend Dev" site="maxpfrontend.ru">
+    <Baby />
+  </App>
+}
+
+export { RoutedApp }
